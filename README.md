2345071ec99ba6657539874305d5b6113397aa20
Kris Nóva <kris@nivenly.com>
2021-01-05 14:05:53 -0800

    linked list
    Signed-off-by: Kris Nóva <kris@nivenly.com>

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # job2021
 Nova doing hacks for interviews
+
+
+## Must Know
+
+ - Linked Lists
+ - Binary Trees
+ - Tries
+ - Stacks
+ - Queues
+ - Vectors / ArrayLists
+ - Hash Tables
+ - Breadth First Search
+ - Depth First Search
+ - Binary Search
+ - Merge Sort
+ - Quick Sort
+ - Tree Insert / Find / etc
+ - Bit Manipulation
+ - Singleton
+ - Factor
+ - Memory (Stack vs Heap)
+ - Recursion
+ - O(n)
+ - Dynamic Programming
+ 
+ 
---
+++ cs/structure_linkedlist.go
@@ -0,0 +1,68 @@
+package main
+
+import "fmt"
+
+// Implement a LinkedList of 10 nodes in Go
+func main() {
+	list := NewEmptyList(10)
+	fmt.Println("Len: ", list.len)
+	newNode := &Node{}
+	InsertAt(list, newNode, 7)
+	fmt.Println("Len: ", list.len)
+}
+
+// Creates a new empty list
+func NewEmptyList(size int) *LinkedList {
+	root := &LinkedList{}
+	var current *Node
+	// 0 - 10 == 9 items + 1 root = 10 total
+	j := 0
+	for i := 0; i < size; i++ {
+		new := &Node{}
+		if i == 0 {
+			root.next = new
+		} else {
+			current.next = new
+		}
+		current = new
+		j++
+	}
+	// The reason I would do this in prod is because of course
+	// someone is going to change the code in the loop
+	root.len = j
+	return root
+}
+
+// Q: Since this is a pointer do we have to return anything?
+func InsertAt(list *LinkedList, node *Node, pos int) {
+	if pos == 0 {
+		// TODO We need a new LinkedList
+		return
+	}
+	var current *Node
+	for i := 0; i < list.len; i++ {
+		if i == 0 {
+			// TODO we also have the root here
+			current = list.next
+		} else if i == pos {
+			// Insert element and increase length
+			node.next = current.next
+			current = node
+			list.len++
+		} else {
+			current = current.next
+		}
+	}
+}
+
+// Linked Lists do not have array like IDs you can look up (but do have names, content, etc)
+// Linked Lists only have next* and not previous*
+type Node struct {
+	next *Node
+}
+
+// Basically just a Node but with a len
+type LinkedList struct {
+	next *Node
+	len  int
+}
